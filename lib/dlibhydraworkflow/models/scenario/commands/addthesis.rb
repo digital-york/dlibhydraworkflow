4164690c16ce91ffa326c20bc285e28b67a6fba8
Frank <frank.feng@york.ac.uk>
2016-08-08 14:39:37 +0100

    update

--- lib/dlibhydraworkflow/models/scenario/commands/addthesis.rb
+++ lib/dlibhydraworkflow/models/scenario/commands/addthesis.rb
@@ -4,12 +4,21 @@ require 'hydra/works'
 module Dlibhydraworkflow
 
   class Addthesis
-
-    def self.execute(parent, preflabel, author, abstract)
+    # create a new thesis object
+    def self.create_new(preflabel, author, abstract)
       thesis = Dlibhydra::Thesis.create(:preflabel => preflabel,:author => author, :abstract => abstract)
-#      parent.members << thesis
-#      parent.save!
       thesis.save!
+      thesis
+    end
+
+    # create a new thesis object under a given collection
+    def self.create_new_within_collection(parent_id, preflabel, author, abstract)
+      parent = Dlibhydra::Collection.find(parent_id)
+      thesis = Dlibhydra::Thesis.create(:preflabel => preflabel,:author => author, :abstract => abstract)
+      parent.members << thesis
+      parent.save!
+      thesis.save!
+      thesis
     end
 
   end
--- spec/models/scenario/commands/addthesis_spec.rb
+++ spec/models/scenario/commands/addthesis_spec.rb
@@ -3,15 +3,25 @@ require 'dlibhydraworkflow'
 
 describe Dlibhydraworkflow::Addthesis do
   describe ".execute" do
-   context "given parameters" do
-      it "returns thesis object" do
-#        collection = Dlibhydra::Collection.find('73/ad/13/b5/73ad13b5-71c6-4a7f-a8f9-ff5adff12b13')
+   context "given preflabel, author, and abstract" do
+      it "returns a new thesis object" do
         preflabel  = 'Thesis test'
         author     = 'Frank Feng'
         abstract   = 'Thesis abstract text'
-        thesis = Dlibhydraworkflow::Addthesis.execute(nil, preflabel, author, abstract)
-puts thesis.inspect
+        thesis = Dlibhydraworkflow::Addthesis.create_new(preflabel, author, abstract)
+        expect(thesis).to be_truthy
       end
     end
+
+#   context "given parent id, preflabel, author, abstract" do
+#      it "returns a new thesis object within the collection" do
+#        parent_id  = '73/ad/13/b5/73ad13b5-71c6-4a7f-a8f9-ff5adff12b13'
+#        preflabel  = 'Thesis test'
+#        author     = 'Frank Feng'
+#        abstract   = 'Thesis abstract text'
+#        thesis = Dlibhydraworkflow::Addthesis.create_new_within_collection(parent_id, preflabel, author, abstract)
+#        expect(thesis).to be_truthy  
+#      end
+#    end
   end
 end
